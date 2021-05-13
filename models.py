14262fe3ae174bd3e6aa3ce4ae15e5037373d736
Cleber Feijo <cleberfeijo@id.uff.br>
2021-05-13 18:59:23 -0300

    Programa que guarda Programadores e suas Habilidades em um banco de dados, feito com REST API utilizando Flask. Em models foi criado uma tabela de usuários com login e senha para autenticação nas APIs.

---
+++ models.py
@@ -0,0 +1,82 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import scoped_session, sessionmaker, relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('sqlite:///atividades.db', convert_unicode=True)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         bind=engine))
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+class Programador(Base):
+    __tablename__ = 'programador'
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(40), index=True)
+    idade = Column(Integer)
+    email = Column(String)
+
+    def __repr__(self):
+        return'<Programador {}>'.format(self.nome, self.idade, self.email)
+
+    def save(self):
+        db_session.add(self)
+        db_session.commit()
+    def delete(self):
+        db_session.delete(self)
+        db_session.commit()
+
+class Habilidades(Base):
+    __tablename__ = 'habilidades'
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(40), index=True)
+    def __repr__(self):
+        return'<Habilidades {}>'.format(self.nome)
+
+    def save(self):
+        db_session.add(self)
+        db_session.commit()
+    def delete(self):
+        db_session.delete(self)
+        db_session.commit()
+
+class ProgHab(Base):
+    __tablename__ = 'proghab'
+    id = Column(Integer, primary_key=True)
+    habilidades_id = Column(Integer, ForeignKey('habilidades.id'))
+    habilidades = relationship("Habilidades")
+    programador_id = Column(Integer, ForeignKey('programador.id'))
+    programador = relationship("Programador")
+    def __repr__(self):
+        return'{}, {}'.format(self.programador, self.habilidades)
+
+    def save(self):
+        db_session.add(self)
+        db_session.commit()
+
+    def delete(self):
+        db_session.delete(self)
+        db_session.commit()
+
+class Usuarios(Base):
+    __tablename__='usuarios'
+    id = Column(Integer, primary_key=True)
+    login = Column(String(20), unique=True)
+    senha = Column(String(20))
+
+    def __repr__(self):
+        return '<Usuario {}>'.format(self.login)
+
+    def save(self):
+        db_session.add(self)
+        db_session.commit()
+
+    def delete(self):
+        db_session.delete(self)
+        db_session.commit()
+
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
+
+if __name__ == '__main__':
+    init_db()
---
+++ utils.py
@@ -0,0 +1,89 @@
+from models import Programador, Habilidades, ProgHab, Usuarios
+
+#Insere Dados na Tabela Programador
+def insere_programador():
+    pessoa = Programador(nome='Cleber', idade=22, email='cleberfeijo@hotmail.com')
+    print(pessoa)
+    pessoa.save()
+
+#Realiza consulta na Tabela Programador
+def consulta_programador():
+    pessoa = Programador.query.all()
+    print(pessoa)
+    pessoas = Programador.query.filter_by(nome='Renato').first()
+    print(pessoas.idade)
+    pessoase = Programador.query.filter_by(nome='Renato').first()
+    print(pessoase.email)
+
+#Altera dados na Tabela Programador
+def altera_programador():
+    pessoa = Programador.query.filter_by(nome='Renato').first()
+    pessoa.idade = 29
+    pessoa.save()
+
+#Exclui dados na Tabela Programador
+def exclui_programador():
+    pessoa = Programador.query.filter_by(nome='Cleber').first()
+    pessoa.delete()
+
+#Insere Dados na Tabela Habilidades
+def insere_habilidade():
+    hab = Habilidades(nome='C/C++')
+    print(hab)
+    hab.save()
+
+#Realiza consulta na Tabela Habilidades
+def consulta_habilidade():
+    lep = Habilidades.query.all()
+    print(lep)
+    hab = Habilidades.query.filter_by(nome='Python').first()
+    print(hab.nome)
+
+#Altera dados na Tabela Habilidades
+def altera_habilidade():
+    hab = Habilidades.query.filter_by(nome='C/C++').first()
+    hab.nome = 'Ruby'
+    hab.save()
+
+#Exclui dados na Tabela Habilidades
+def exclui_habilidade():
+    hab = Habilidades.query.filter_by(nome='Ruby').first()
+    hab.delete()
+
+#Consulta o Programador, sua Idade e sua Habilidade
+def consulta():
+    hab = Habilidades.query.filter_by(nome='C/C++').first()
+    pessoa = Programador.query.filter_by(nome='Renato').first()
+    print(pessoa.nome, pessoa.idade, hab.nome)
+
+def exclui_tudo():
+    hab = ProgHab.query.filter_by(id=4).first()
+    hab.delete()
+
+def printar():
+    hab = ProgHab.query.all()
+    print(hab)
+
+def insere_usuario(login, senha):
+    usuario = Usuarios(login=login, senha=senha)
+    usuario.save()
+
+def consulta_usuarios():
+    usuarios = Usuarios.query.all()
+    print(usuarios)
+
+if __name__ == '__main__':
+    insere_usuario('rafael', '1234')
+    insere_usuario('cleber', '4321')
+    consulta_usuarios()
+    insere_programador()
+    exclui_programador()
+    altera_programador()
+    consulta_programador()
+    insere_habilidade()
+    altera_habilidade()
+    exclui_habilidade()
+    consulta_habilidade()
+    consulta()
+    #exclui_tudo()
+    printar()
